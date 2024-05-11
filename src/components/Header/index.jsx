e9908ae021836ca994d070ef07513dddc4d0a98e
titenq <titenq@gmail.com>
2024-05-11 09:13:11 -0300

    responsive header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,71 +1,26 @@
-import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { Container, Image, Nav, Navbar } from 'react-bootstrap';
+import { Container, Image, Navbar } from 'react-bootstrap';
 
 import styles from './Header.module.css';
 import logo from '../../assets/img/logo-javascript-256.png';
 
 const Header = () => {
   const navigate = useNavigate();
-  const [login, setLogin] = useState(false);
 
   return (
     <Navbar className={styles.navbar} sticky="top">
       <Container>
         <Navbar.Brand onClick={() => navigate('/')}>
           <Image
-            width={128}
-            height={128}
+            width={50}
+            height={50}
             src={logo}
             fluid
             className={styles.logo_header}
           />
         <span className={styles.sis}>Javascript</span>
         </Navbar.Brand>
-        
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className={`ms-auto ${styles.font}`} defaultActiveKey="/">
-            <NavLink
-              className={styles.links}
-              to="/"
-            >
-              Home
-            </NavLink>
-
-            {!login && (
-              <NavLink
-                className={styles.links}
-                to="/registrar"
-              >
-                Registrar
-              </NavLink>
-            )}
-
-            {!login && (
-              <NavLink
-                className={styles.links}
-                to="/login"
-              >
-                Login
-              </NavLink>
-            )}
-            {login && (
-              <NavLink
-                className={styles.links}
-                to="/"
-              >
-                Sair
-              </NavLink>
-            )}
-          </Nav>
-
-          {/* {login && (
-            <div className={styles.avatar}>{avatar}</div>
-          )} */}
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
