f922bc6494d2fb86bb1690bce89991e425ad81e7
titenq <titenq@gmail.com>
2024-05-31 18:28:29 -0300

    react-ga4

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactGA from 'react-ga4';
 
 import App from './App.jsx';
 import './index.css';
+
+ReactGA.initialize('G-FJ7RFRTS7X');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
