4c8374016373835ac96c44a80c2c9f927e731a32
TitenQ <titenq@gmail.com>
2025-07-29 08:56:23 -0300

    hello world in c

--- src/helpers/code/helloWorld.js
+++ src/helpers/code/helloWorld.js
@@ -1,18 +1,16 @@
 const helloWorld = {
   '01':
-`int main() 
-{ 
-  printf("Hello World!\\n");
-  
-  return 0; 
+`#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\\n");
+  return 0;
 }
 `,
 
   '02':
-`public class HelloWorld
-{ 
-  public static void main(String[] args)
-  { 
+`public class HelloWorld { 
+  public static void main(String[] args) { 
     System.out.println("Hello, World!"); 
   } 
 }
